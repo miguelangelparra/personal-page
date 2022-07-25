7bce6377fa8134771e473efe25a1e97b1ddf7796
miguelangelparra <miguelangel.parradavila@gmail.com>
2022-07-25 07:35:20 -0300

    fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,7 +9,7 @@ export default function Document() {
           rel="stylesheet"
         />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com"  />
         <link
           href="https://fonts.googleapis.com/css2?family=Electrolize&display=swap"
           rel="stylesheet"
