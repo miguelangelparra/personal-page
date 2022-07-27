43d9b40ed99edc5a1e92de4fd68bffc062aa0f92
miguelangelparra <miguelangel.parradavila@gmail.com>
2022-07-27 00:55:53 -0300

    Feat: GA in variables
     Changes to be committed:
    	new file:   .env.development
    	new file:   .env.production
    	modified:   pages/_app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,15 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Script from "next/script";
 
+
+const ANALYTICS_ID = process.env.NEXT_PUBLIC_ANALYTICS_ID
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Script
         strategy="afterInteractive"
-        src="https://www.googletagmanager.com/gtag/js?id=G-QVP2ZDL7KK"
+        src={`https://www.googletagmanager.com/gtag/js?id=${ANALYTICS_ID}`}
       />
       <Script id="google-analytics" strategy="afterInteractive">
         {`
@@ -15,7 +18,7 @@ function MyApp({ Component, pageProps }: AppProps) {
          function gtag(){dataLayer.push(arguments);}
          gtag('js', new Date());
        
-         gtag('config', 'G-QVP2ZDL7KK');
+         gtag('config', '${ANALYTICS_ID}');
         `}
       </Script>
       <Component {...pageProps} />
