90a124856d3ae20877952038d2a5198e8e8c5896
miguelangelparra <miguelangel.parradavila@gmail.com>
2022-07-31 22:04:58 -0300

    fix: icons and contestar  On branch hotfix/0.0.4  Changes to be committed: 	modified:   components/ui/ExperienceCard.tsx   modified:   components/ui/Sidebar.tsx 	modified:   pages/about/index.tsx 	modified:   pages/contact/index.tsx

--- components/ui/ExperienceCard.tsx
+++ components/ui/ExperienceCard.tsx
@@ -1,4 +1,4 @@
-import { Experience } from '../../interfaces/';
+import { Experience } from "../../interfaces/";
 import {
   Card,
   CardActionArea,
@@ -8,36 +8,39 @@ import {
 } from "@mui/material";
 
 interface Props {
- experience: Experience
+  experience: Experience;
 }
 
-export const ExperienceCard = ({experience}:Props) => {
+export const ExperienceCard = ({ experience }: Props) => {
   return (
     <Card sx={{ marginBottom: 1 }}>
       <CardActionArea>
         <CardContent>
           <Typography sx={{ whiteSpace: "pre-line" }}>
-          {experience.title}          </Typography>
+            {experience.title}{" "}
+          </Typography>
           <Typography sx={{ whiteSpace: "pre-line" }}>
-          {experience.company}          </Typography>
-          <Typography sx={{ whiteSpace: "pre-line" }}>
-          {experience.time}          </Typography>
-          <Typography sx={{ whiteSpace: "pre-line" }}>
-          {experience.description}          </Typography>
+            {experience.company}{" "}
+          </Typography>
+          <Typography sx={{ whiteSpace: "pre-line",paddingTop:2 }}>
+            {experience.description}{" "}
+          </Typography>
         </CardContent>
         <CardActions
-          sx={{ display: "flex", justifyContent: "end", paddingRight: 2 }}
+          sx={{ display: "flex", justifyContent: "end", paddingRight: 2}}
         >
-          <Typography variant="body2">Hace 30 minutos</Typography>
+          <Typography variant="body2">{experience.time} </Typography>
         </CardActions>
       </CardActionArea>
     </Card>
   );
 };
 
-{/* <div key={experience.id} className="card">
+{
+  /* <div key={experience.id} className="card">
 <h4>{experience.title}</h4>
 <h5>{`${experience.company} | ${experience.location}`}</h5>
 <h5>{experience.time}</h5>
 <p>{experience.description}</p>
-</div> */}
+</div> */
+}
